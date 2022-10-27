862d8af8497aff477a32738c2875b4be11480597
Kazae67 <Kazae.zaeka@gmail.com>
2022-10-27 09:23:16 +0200

    Quelques test de fonctionnalités

--- livres/index.php
+++ livres/index.php
@@ -18,6 +18,13 @@ $livre2 = new Livres("Les rois des océans", 1850, 250, 7.50, "€", $auteur);
 $auteur2 = new Auteur("Singe", "Wukong");
 $livre = new Livres("Roi des singes", 1287, 750, 28.99, "€", $auteur2);
 
+$auteur3 = new Auteur("Nicolas", "Sarokozy");
+$livre = new Livres("Mettre le monde à sa poche", 2008, 89, 4.60, "$", $auteur3);
+$livre2 = new Livres("Séduire la France", 2008, 103, 5.50, "€", $auteur3);
+$livres3 = new Livres("Trahison", 2007, 77, 2.40, "€", $auteur3);
+$livres4 = new Livres("Faciliter le pouvoir d'achat", 2004, 42, 7.50, "€", $auteur3);
+
 echo $auteur->afficherBibliographie();
 echo $auteur2->afficherBibliographie();
+echo $auteur3->afficherBibliographie();
 ?>
